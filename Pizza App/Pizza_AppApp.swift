0bd964f5eeace8c1c1afaa8aea8f28cccce57e40
Jeevan Joshi <jeevanc@geekyants.com>
2023-02-26 17:38:29 +0530

    Initial Commit

---
+++ Pizza App/Pizza_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_AppApp.swift
+//  Pizza App
+//
+//  Created by Jeevan Chandra Joshi on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
